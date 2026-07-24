faf2f9908cf5c722c46f2e16282187d407f75e9d
William Izquierdo <izquierdowaws@gmail.com>
2026-07-24 09:28:39 -0400

    Init

--- backend/lambda_handler.py
+++
@@ -1,5 +0,0 @@
-"""Handler Lambda usando Mangum para despliegue en AWS Lambda."""
-from mangum import Mangum
-from app.main import app
-
-handler = Mangum(app, lifespan="off")
--- frontend/android/app/src/main/kotlin/com/babyhealth/babyhealth/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.babyhealth.babyhealth
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
